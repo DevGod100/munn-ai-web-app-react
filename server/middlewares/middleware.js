9b9c2e60b39bd1cba3e1d7e1927ff7a241e7ea1c
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2023-10-04 19:19:37 +0300

    hide node modules

--- server/middlewares/middleware.js
+++
@@ -1,8 +0,0 @@
-const multer = require('multer');
-
-// Multer setup and other middleware functions here
-
-module.exports = {
-  uploadMiddleware: multer(/* configuration */),
-  // Other middleware functions
-};
